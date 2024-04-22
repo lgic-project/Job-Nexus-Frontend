f88536725d8c04953a4988314260417960d02f2c
Binaya9898 <82639575+Binaya9898@users.noreply.github.com>
2024-04-22 15:12:26 +0545

    fafnsf

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, View } from "react-native";
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
       <View>
-        <Text>I am aakrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
+        <Text>I am bikrity </Text>
       </View>
-    )
+    );
   }
 }
