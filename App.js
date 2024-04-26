319574540bbaa5d8fbe212bcaa8ed09d62c444c8
Binaya9898 <82639575+Binaya9898@users.noreply.github.com>
2024-04-26 16:17:21 +0545

    Merge Conflicts refined

--- App.js
+++ App.js
@@ -1,69 +1,35 @@
-<<<<<<< HEAD
-import { Text, View } from "react-native";
-import React, { Component } from "react";
-
-export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-        <Text>I am bikrity </Text>
-      </View>
-    );
-  }
-}
-=======
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Login, Signup, Welcome } from "./screens";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Welcome'
-      >
+      <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen
           name="Welcome"
           component={Welcome}
           options={{
-            headerShown: false
+            headerShown: false,
           }}
         />
         <Stack.Screen
           name="Login"
           component={Login}
           options={{
-            headerShown: false
+            headerShown: false,
           }}
         />
         <Stack.Screen
           name="Signup"
           component={Signup}
           options={{
-            headerShown: false
+            headerShown: false,
           }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
->>>>>>> d6a0fc39c1a8b91b0f8a882027cd35b9cb0cae93
