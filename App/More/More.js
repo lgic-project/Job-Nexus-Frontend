2624d43fdbe693429caeeeba1ff6cb6879fc968b
Binaya9898 <82639575+Binaya9898@users.noreply.github.com>
2024-05-11 14:21:52 +0545

    Api Fetching Test Succeed

--- App/More/More.js
+++ App/More/More.js
@@ -1,12 +1,44 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default class Favourite extends Component {
-    render() {
-        return (
-            <View>
-                <Text>This is a More Page</Text>
-            </View>
-        )
-    }
-}
+const More = () => {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("Fetching");
+        const response = await axios.get("http://127.0.0.1:8000/api/job-api");
+        console.log(jobs);
+        setJobs(response.data);
+        console.log(jobs);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+    console.log("Jobs Fetched ", jobs);
+  }, []);
+  const renderItem = ({ item }) => (
+    <View>
+      {/* style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: "#ccc" }} */}
+      <Text>{item.job_title}</Text>
+      <Text>{item.job_description}</Text>
+      <Text>{item.job_category}</Text>
+      <Text>{item.job_address}</Text>
+    </View>
+  );
+  return (
+    <View>
+      <Text>Jobs List</Text>
+      <FlatList
+        data={jobs}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
+  );
+  return <Text>More Page</Text>;
+};
+
+export default More;
